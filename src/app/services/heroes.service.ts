db968cc173f0b1b491a0b72eccf5036053ab5b14
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2019-04-09 00:37:33 -0400

    lista la aplicación SPA

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -68,13 +68,18 @@ export class HeroesService {
     return this.heroes[idx];
   }
 
-  buscarHeroe( termino:string ):Heroe[] {
+  buscarHeroes( termino:string ):Heroe[] {
     let heroesArr:Heroe[] = [];
     termino = termino.toLowerCase();
 
-    for( let heroe of this.heroes ){
+    for( let i = 0; i < this.heroes.length; i++ ){
+
+      let heroe = this.heroes[i];
+
       let nombre = heroe.nombre.toLowerCase();
+
       if ( nombre.indexOf( termino ) >= 0 ) {
+        heroe.idx = i;
         heroesArr.push(heroe);
       }
     }
@@ -91,4 +96,5 @@ export interface Heroe{
   img: string;
   aparicion: string;
   casa: string;
+  idx?: number; //opcional
 };
